421e3a416cd8b78ba8af4f87a3d7793aa71ccfb2
nabeerdar <52133097+nabeerdar@users.noreply.github.com>
2022-05-02 15:20:11 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Muhammad Sajid Qureshi 99-FBAS\/PHDCS\/F13 65042bac</title>
+</head>
+<body>
+    <div >
+        <h1>Welcome to Autos Database</h1>
+        <p>
+            <a href="login.php">Please Log In</a>
+        </p>
+        <p>
+            Attempt to go to
+            <a href="autos.php">autos.php</a> without logging in - it should fail with an error message.
+  
+     
+    </div>
+</body>
